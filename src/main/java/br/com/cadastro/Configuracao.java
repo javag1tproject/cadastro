7f9cc5d6948f7ae444b7fe5b262ecf486c7e3770
Vinícius Júnior <vinicius.junior@pcservice.com.br>
2018-01-10 08:56:00 -0300

    Commit - Evolutioon

--- src/main/java/br/com/cadastro/Configuracao.java
+++ src/main/java/br/com/cadastro/Configuracao.java
@@ -1,7 +1,11 @@
 package br.com.cadastro;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.stereotype.Controller;
 
 
@@ -13,5 +17,16 @@ public class Configuracao {
 	   
 	public static void main(String[] args){
 	    SpringApplication.run(Configuracao.class, args);
+	}
+	
+	
+	@Bean
+	public DataSource dataSource(){
+	    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+	    dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+	    dataSource.setUrl("jdbc:mysql://localhost:3306/alohana");
+	    dataSource.setUsername("root");
+	    dataSource.setPassword("admin");
+	    return dataSource;
 	}
 }
